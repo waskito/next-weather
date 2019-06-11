bb0fc8ab71959596068dbc08d6b32bc80c06abae
Yanuar Waskito <yanuar.gws@gmail.com>
2019-06-11 09:15:11 +0700

    Use geolocation to determine location

---
+++ components/button-geolocation.js
@@ -0,0 +1,64 @@
+import { bindActionCreators, compose } from 'redux'
+import { connect } from 'react-redux'
+import { get, isEmpty, isEqual } from 'lodash'
+import { geolocated } from 'react-geolocated'
+import {
+  getWeatherByLatLng,
+  saveLocation,
+  setLocation
+} from '../stores/actions'
+import React from 'react'
+
+class ButtonGeolocation extends React.Component {
+
+  componentDidUpdate( prevProps, prevState ) {
+    if ( this.props.coords != null ){
+      const {
+        coords,
+        getWeatherByLatLng,
+        saveLocation,
+        setLocation
+      } = this.props;
+      const lat = get(coords, 'latitude');
+      const lng = get(coords, 'longitude');
+      saveLocation(lat, lng, 'geolocation');
+      setLocation(lat, lng, 'geolocation');
+      getWeatherByLatLng(lat, lng);
+    }
+  }
+
+  render() {
+    const {
+      coords,
+      isGeolocationAvailable,
+      isGeolocationEnabled
+    } = this.props;
+    return (
+      <div>
+        <p>{ isGeolocationAvailable ? 'available' : 'unavailable' }</p>
+        <p>{ isGeolocationEnabled ? 'enabled' : 'disabled' }</p>
+        <p>{ coords ? `allowed (lat,lng): ${coords.latitude}, ${coords.longitude} ` : 'unallowed' }</p>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({});
+const mapDispatchToProps = dispatch => ({
+  getWeatherByLatLng: bindActionCreators( getWeatherByLatLng, dispatch ),
+  saveLocation: bindActionCreators( saveLocation, dispatch ),
+  setLocation: bindActionCreators( setLocation, dispatch )
+})
+
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  geolocated({
+    positionOptions: {
+      enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000
+  })
+)(ButtonGeolocation);
